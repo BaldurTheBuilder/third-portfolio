fdc66951f79fb3f2ce1f1c6edd48e86629431c21
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-07-19 22:16:17 -0700

    server appears to be working, project 3 info in models and schemas

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+// Options should not be needed with mongoose v7^
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/jackPortfolio', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,60 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    description: String
+    skills: [String]
+    createdTasks: [Task]
+    assignedTasks: [Task]
+    watchedTasks: [Task]
+    # fundedTasks: [FundingInfo]
+    fundedTasks: [Task]
+  }
+
+  # type FundingInfo {
+  #   fundedTask: Task
+  #   funding: String
+  # }
+
+  type FundingUsers {
+    username: String
+    funding: String
+  }
+
+  type Task {
+    _id: ID
+    taskAuthor: String
+    assignedUser: String
+    taskName: String
+    description: String
+    currentFunding: String
+    watchingUsers: [String]
+    fundingUsers: [FundingUsers]
+  }
+
+  type Query {
+    users: [User]!
+    tasks: [Task]!
+    unclaimedTasks: [Task]
+    user(userId: ID!): [User]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!, firstName: String!): Auth
+    login(email: String!, password: String!): Auth
+    addTask(taskName: String!, description: String!): Task
+  }
+`;
+
+module.exports = typeDefs;
