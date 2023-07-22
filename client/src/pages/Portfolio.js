e6049becd0056af70e3f3fc26e9b1aefccd82d5f
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-07-21 18:29:13 -0700

    project seed under construction, card component is a mess

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // should include a series of project cards
+import Card from '../components/Card/Card'
 // each card includes a small image of the project, the title, a brief description, and a link to either the live project or the github repository.
 
 
