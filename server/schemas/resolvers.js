d1413e3814b27e80d97a0f16d981549965301479
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-07-21 19:23:22 -0700

    db seeding strings. No images or arrays.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,73 +1,73 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Task } = require("../models");
+const { User, Project } = require("../models");
 // maybe change auth.js to index.js for simple imports
-const { signToken } = require('../auth/auth');
+// const { signToken } = require('../auth/auth');
 
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find()
-      .populate('createdTasks')
-      .populate('assignedTasks')
-      .populate('watchedTasks')
-      .populate('fundedTasks');
+    // users: async () => {
+    //   return User.find()
+    //   .populate('createdTasks')
+    //   .populate('assignedTasks')
+    //   .populate('watchedTasks')
+    //   .populate('fundedTasks');
+    // },
+    // user: async (parent, {userId}) => {
+    //   return User.find({_id: userId})
+    //   .populate('createdTasks')
+    //   .populate('assignedTasks')
+    //   .populate('watchedTasks')
+    //   .populate('fundedTasks');
+    // },
+    projects: async () => {
+      return Project.find();
     },
-    user: async (parent, {userId}) => {
-      return User.find({_id: userId})
-      .populate('createdTasks')
-      .populate('assignedTasks')
-      .populate('watchedTasks')
-      .populate('fundedTasks');
-    },
-    tasks: async () => {
-      return Task.find();
-    },
-    unclaimedTasks: async () => {
-      const params = {assignedUser: null};
-      return Task.find(params);
-    },
+    // unclaimedTasks: async () => {
+    //   const params = {assignedUser: null};
+    //   return Task.find(params);
+    // },
   },
-  Mutation: {
-    addUser: async (parent, { username, email, password, firstName }) => {
-      const user = await User.create({ username, email, password, firstName });
-      const token = signToken(user);
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+  // Mutation: {
+  //   addUser: async (parent, { username, email, password, firstName }) => {
+  //     const user = await User.create({ username, email, password, firstName });
+  //     const token = signToken(user);
+  //     return { token, user };
+  //   },
+  //   login: async (parent, { email, password }) => {
+  //     const user = await User.findOne({ email });
 
-      if (!user) {
-        throw new AuthenticationError('No user found with this email address');
-      }
+  //     if (!user) {
+  //       throw new AuthenticationError('No user found with this email address');
+  //     }
 
-      const correctPw = await user.isCorrectPassword(password);
+  //     const correctPw = await user.isCorrectPassword(password);
 
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
+  //     if (!correctPw) {
+  //       throw new AuthenticationError('Incorrect credentials');
+  //     }
 
-      const token = signToken(user);
+  //     const token = signToken(user);
 
-      return { token, user };
-    },
-    addTask: async (parent, { taskName, description }, context) => {
-      if(context.user) {
-        const task = await Task.create({
-          taskName,
-          description,
-          taskAuthor: context.user.username
-        });
+  //     return { token, user };
+  //   },
+  //   addTask: async (parent, { taskName, description }, context) => {
+  //     if(context.user) {
+  //       const task = await Task.create({
+  //         taskName,
+  //         description,
+  //         taskAuthor: context.user.username
+  //       });
 
-        await User.findOneAndUpdated(
-          { _id: context.user._id },
-          { $pull: { createdTasks: task._id }}
-        );
+  //       await User.findOneAndUpdated(
+  //         { _id: context.user._id },
+  //         { $pull: { createdTasks: task._id }}
+  //       );
 
-        return task;
-      }
-    }
-  },
+  //       return task;
+  //     }
+  //   }
+  // },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,60 +1,59 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    firstName: String
-    lastName: String
-    email: String
-    password: String
-    description: String
-    skills: [String]
-    createdTasks: [Task]
-    assignedTasks: [Task]
-    watchedTasks: [Task]
-    # fundedTasks: [FundingInfo]
-    fundedTasks: [Task]
-  }
+  # type User {
+  #   _id: ID
+  #   username: String
+  #   firstName: String
+  #   lastName: String
+  #   email: String
+  #   password: String
+  #   description: String
+  #   skills: [String]
+  #   # createdTasks: [Task]
+  #   # assignedTasks: [Task]
+  #   # watchedTasks: [Task]
+  #   # # fundedTasks: [FundingInfo]
+  #   # fundedTasks: [Task]
+  # }
 
   # type FundingInfo {
   #   fundedTask: Task
   #   funding: String
   # }
 
-  type FundingUsers {
-    username: String
-    funding: String
-  }
+  # type FundingUsers {
+  #   username: String
+  #   funding: String
+  # }
 
-  type Task {
+  type Project {
     _id: ID
-    taskAuthor: String
-    assignedUser: String
-    taskName: String
-    description: String
-    currentFunding: String
-    watchingUsers: [String]
-    fundingUsers: [FundingUsers]
+    projectTitle: String
+    briefDescription: String
+    techUsed: [String]
+    detailedDescription: String
+    repoLink: String
+    liveLink: String
   }
 
   type Query {
-    users: [User]!
-    tasks: [Task]!
-    unclaimedTasks: [Task]
-    user(userId: ID!): [User]!
+    # users: [User]!
+    projects: [Project]!
+    # unclaimedTasks: [Task]
+    # user(userId: ID!): [User]!
   }
 
-  type Auth {
-    token: ID!
-    user: User
-  }
+  # type Auth {
+  #   token: ID!
+  #   user: User
+  # }
 
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!, firstName: String!): Auth
-    login(email: String!, password: String!): Auth
-    addTask(taskName: String!, description: String!): Task
-  }
+  # type Mutation {
+  #   # addUser(username: String!, email: String!, password: String!, firstName: String!): Auth
+  #   # login(email: String!, password: String!): Auth
+  #   # addTask(taskName: String!, description: String!): Task
+  # }
 `;
 
 module.exports = typeDefs;
